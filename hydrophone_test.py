1950aaf7b82bebbab5d31bf0c9d15ae4e32d6649
raijjjinn <emhutch003@gmail.com>
2024-10-24 16:17:53 -0400

    reformat with black

--- hydrophone_test.py
+++ hydrophone_test.py
@@ -10,20 +10,26 @@ from receiver import record_sound, on_off_keying
 
 CHUNK = 2**5  # bytes of data per chunk of the audio signal
 RATE = 44100  # sample rate in kHz
-MSGLEN = 10  # length of msg. in seconds. will eventually need to switch to be continuous
+MSGLEN = (
+    10  # length of msg. in seconds. will eventually need to switch to be continuous
+)
 
-message_map = {"1011":"wav_files/Low_O2.wav",
-        "1001":"wav_files/Return_to_Boat.wav",
-        "1110":"wav_files/Look_at_This.wav",
-        "0011":"wav_files/SOS.wav",}
+message_map = {
+    "1011": "wav_files/Low_O2.wav",
+    "1001": "wav_files/Return_to_Boat.wav",
+    "1110": "wav_files/Look_at_This.wav",
+    "0011": "wav_files/SOS.wav",
+}
 
 EVAN = "0101"
 SARAH = "1010"
 SEB = "0110"
 
-id_dict = {EVAN:"wav_files/Evan_ID.wav",
-           SARAH:"wav_files/Sarah_ID.wav",
-           SEB:"wav_files/Seb_ID.wav",}
+id_dict = {
+    EVAN: "wav_files/Evan_ID.wav",
+    SARAH: "wav_files/Sarah_ID.wav",
+    SEB: "wav_files/Seb_ID.wav",
+}
 
 
 def sender():
@@ -31,10 +37,10 @@ def sender():
     SARAH = "1010"
     SEB = "0110"
 
-
     single_packet = ID_1 + message_map["LOW OXYGEN"]
-    
+
     binary_to_tones(single_packet)
+
 
 # sender()
 
@@ -59,16 +65,16 @@ for line in f:
     for phrase in keep_phrases:
         if phrase in line and "Port" not in line:
             important.append(line)
-            break # NOTE is hthis break necessary?
+            break  # NOTE is hthis break necessary?
 print(important)
 
 for line in important:
-    '''split message, take 1st index to get part with data, 
-    take 11th-2nd to last index to get binary then make it 
-    into a string'''
+    """split message, take 1st index to get part with data,
+    take 11th-2nd to last index to get binary then make it
+    into a string"""
 
-    message = re.split("\x1b",line)[1][11:-1]
-    print("MESSAGE: ",message)
+    message = re.split("\x1b", line)[1][11:-1]
+    print("MESSAGE: ", message)
     msg_str = ""
     for char in message:
         if char not in "[],":
@@ -81,48 +87,52 @@ for line in important:
     if phrase in message_map:
         msg_file = message_map[phrase]
         name_id = id_dict[name]
-        sound = wave.open(msg_file,"rb") 
-        identifier = wave.open(name_id,'rb')
-        
-        p = pyaudio.PyAudio()  
-        #open stream  
-        stream = p.open(format = p.get_format_from_width(identifier.getsampwidth()),  
-                        channels = identifier.getnchannels(),  
-                        rate = identifier.getframerate(),  
-                        output = True)  
-        #read data  
-        data = identifier.readframes(CHUNK)  
-        
-        #play stream  
-        while data:  
-            stream.write(data)  
-            data = identifier.readframes(CHUNK)  
-        
-        #stop stream  
-        stream.stop_stream()  
-        stream.close()  
-        p.terminate() 
+        sound = wave.open(msg_file, "rb")
+        identifier = wave.open(name_id, "rb")
 
-        p = pyaudio.PyAudio()  
-        #open stream  
-        stream = p.open(format = p.get_format_from_width(sound.getsampwidth()),  
-                        channels = sound.getnchannels(),  
-                        rate = sound.getframerate(),  
-                        output = True)  
-        #read data  
-        data = sound.readframes(CHUNK)  
-        
-        #play stream  
-        while data:  
-            stream.write(data)  
-            data = sound.readframes(CHUNK)  
-        
-        #stop stream  
-        stream.stop_stream()  
-        stream.close()  
-        
-        #close PyAudio  
-        p.terminate()  
+        p = pyaudio.PyAudio()
+        # open stream
+        stream = p.open(
+            format=p.get_format_from_width(identifier.getsampwidth()),
+            channels=identifier.getnchannels(),
+            rate=identifier.getframerate(),
+            output=True,
+        )
+        # read data
+        data = identifier.readframes(CHUNK)
+
+        # play stream
+        while data:
+            stream.write(data)
+            data = identifier.readframes(CHUNK)
+
+        # stop stream
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+        p = pyaudio.PyAudio()
+        # open stream
+        stream = p.open(
+            format=p.get_format_from_width(sound.getsampwidth()),
+            channels=sound.getnchannels(),
+            rate=sound.getframerate(),
+            output=True,
+        )
+        # read data
+        data = sound.readframes(CHUNK)
+
+        # play stream
+        while data:
+            stream.write(data)
+            data = sound.readframes(CHUNK)
+
+        # stop stream
+        stream.stop_stream()
+        stream.close()
+
+        # close PyAudio
+        p.terminate()
     # if message in {[1, 0, 0, 1]: "LOW OXYGEN"}.keys():
     #     engine = pyttsx3.init()
     #     engine.setProperty("rate", 150)
@@ -130,4 +140,3 @@ for line in important:
     #     engine.runAndWait()
 
 # print(important)
-
